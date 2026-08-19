d9b94d21fc6b4bb92b3596dcbfbe3db6e5dfc3f2
DemiSushi <yjtham03@gmail.com>
2026-08-19 10:28:05 +0800

    Week 2 Project 4. Add increment: Level-0

---
+++ src/main/java/CS2113_Bot.java
@@ -0,0 +1,17 @@
+public class CS2113_Bot {
+    public static void main(String[] args) {
+        String banner = "_    _ ______ _      _      ____   __          __  {_} _____  _      _____  \n"
+                + "| |  | |  ____| |    | |    / __ \\  \\ \\        / / | |  __ \\| |    |  __ \\ \n"
+                + "| |__| | |__  | |    | |   | |  | |  \\ \\  /\\  / /  | | |__) | |    | |  | |\n"
+                + "|  __  |  __| | |    | |   | |  | |   \\ \\/  \\/ /   | |  _  /| |    | |  | |\n"
+                + "| |  | | |____| |____| |____| |__| |    \\  /\\  /    | | | \\ \\| |____| |__| |\n"
+                + "|_|  |_|______|______|______|\\____/      \\/  \\/     |_| |_| \\_\\______|_____/ ";
+        System.out.println("__________________________________");
+        System.out.println(banner);
+        System.out.println("\nHello! I'm CS2113_Bot.");
+        System.out.println("What can I do for you?");
+        System.out.println("__________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("__________________________________");
+    }
+}
