1c9fde9fa539c0fa1e96cbd254d3c66bf22ba116
DemiSushi <yjtham03@gmail.com>
2026-08-25 21:43:38 +0800

    Completed Level 1 - Echo

--- src/main/java/CS2113_Bot.java
+++ src/main/java/CS2113_Bot.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class CS2113_Bot {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+
         String banner = "_    _ ______ _      _      ____   __          __  {_} _____  _      _____  \n"
                 + "| |  | |  ____| |    | |    / __ \\  \\ \\        / / | |  __ \\| |    |  __ \\ \n"
                 + "| |__| | |__  | |    | |   | |  | |  \\ \\  /\\  / /  | | |__) | |    | |  | |\n"
@@ -10,6 +15,15 @@ public class CS2113_Bot {
         System.out.println(banner);
         System.out.println("\nHello! I'm CS2113_Bot.");
         System.out.println("What can I do for you?");
+        System.out.println("__________________________________");
+        line = in.nextLine();
+        while(!(line.toLowerCase()).equals("bye")){
+            System.out.println("__________________________________");
+            System.out.println(line);
+            System.out.println("__________________________________");
+            line = in.nextLine();
+        }
+
         System.out.println("__________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("__________________________________");
