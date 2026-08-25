2d5c785809227b78c56152ce9461f08162a4bc56
DemiSushi <yjtham03@gmail.com>
2026-08-25 22:04:14 +0800

    Completed Level-2 - Add, List

--- src/main/java/CS2113_Bot.java
+++ src/main/java/CS2113_Bot.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class CS2113_Bot {
     public static void main(String[] args) {
         String line;
+        String[] my_List = new String[100];
+        int list_Count = 0;
+
         Scanner in = new Scanner(System.in);
 
         String banner = "_    _ ______ _      _      ____   __          __  {_} _____  _      _____  \n"
@@ -19,8 +22,17 @@ public class CS2113_Bot {
         line = in.nextLine();
         while(!(line.toLowerCase()).equals("bye")){
             System.out.println("__________________________________");
-            System.out.println(line);
-            System.out.println("__________________________________");
+            if(line.toLowerCase().equals("list")){
+                for(int i=0;i<list_Count;i++){
+                    System.out.println((i+1) + ". " + my_List[i]);
+                }
+                System.out.println("__________________________________");
+            }
+            else{
+                System.out.println(line);
+                System.out.println("__________________________________");
+                my_List[list_Count++] = line;
+            }
             line = in.nextLine();
         }
 
