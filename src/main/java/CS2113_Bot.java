3c7b82b2877196d2cd6d38ae7df088d99122d0fc
DemiSushi <yjtham03@gmail.com>
2026-08-25 23:05:03 +0800

    Completed Level 3-Mark As Done

--- src/main/java/CS2113_Bot.java
+++ src/main/java/CS2113_Bot.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 public class CS2113_Bot {
+
     public static void main(String[] args) {
         String line;
-        String[] my_List = new String[100];
+        Task[] my_List = new Task[100];
         int list_Count = 0;
 
         Scanner in = new Scanner(System.in);
@@ -23,15 +24,33 @@ public class CS2113_Bot {
         while(!(line.toLowerCase()).equals("bye")){
             System.out.println("__________________________________");
             if(line.toLowerCase().equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for(int i=0;i<list_Count;i++){
-                    System.out.println((i+1) + ". " + my_List[i]);
+                    System.out.println((i+1) + ".[" + my_List[i].getStatusIcon() + "] " + my_List[i].description);
                 }
                 System.out.println("__________________________________");
+            }
+            else if (line.toLowerCase().contains("mark")) {
+                String[] words = line.split(" ");
+                int mark_Index = Integer.parseInt(words[1]) - 1;
+                if(words[0].equals("mark")){
+                    my_List[mark_Index].setDone(true);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(("[" + my_List[mark_Index].getStatusIcon() + "] " + my_List[mark_Index].description));
+                    System.out.println("__________________________________");
+                }
+                else{
+                    my_List[mark_Index].setDone(false);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(("[" + my_List[mark_Index].getStatusIcon() + "] " + my_List[mark_Index].description));
+                    System.out.println("__________________________________");
+                }
+
             }
             else{
                 System.out.println(line);
                 System.out.println("__________________________________");
-                my_List[list_Count++] = line;
+                my_List[list_Count++] = new Task(line);
             }
             line = in.nextLine();
         }
